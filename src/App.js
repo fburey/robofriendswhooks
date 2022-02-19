d3771997d0466846b68474eb6912e9b0d2dff1ed
Franklyn Urey Guardado <franklyn.urey@kiumas.net>
2022-02-18 20:54:50 -0600

    L7 Hooks vs Classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+      const optionsDate = {year: 'numeric', month: '2-digit', day: '2-digit' };
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <h3>{(new Date()).toLocaleTimeString('en-US')}</h3>
+            <h3>{(new Date()).toLocaleTimeString('es-NI')}</h3>
+            <h3>{(new Date()).toLocaleDateString('en-US')}</h3>
+            <h3>{(new Date()).toLocaleDateString('es-NI')}</h3>
+            <h3>{(new Date()).toLocaleDateString('en-US',optionsDate)}</h3>
+            <h3>{(new Date()).toLocaleDateString('es-NI',optionsDate)}</h3>
+
+          </header>
+        </div>
+      );
+  }
 }
 
 export default App;
