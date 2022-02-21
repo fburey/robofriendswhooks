2a071bc8a3702fd709eda46194a3904482798a7e
Franklyn Urey Guardado <franklyn.urey@kiumas.net>
2022-02-21 14:24:51 -0600

    L17 Building A React App 4 Parte Final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css'
 
 // const state = {
@@ -10,38 +9,42 @@ import './App.css'
 // }
 
 class App extends Component {
-    
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-        console.log('constructor');
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+    console.log("constructor");
+  }
 
-    componentDidMount(){
-      this.setState({robots: robots});
-      console.log('componentDidMount');
-    }
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+    console.log("componentDidMount");
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name
-            .toLocaleLowerCase()
-            .includes(this.state.searchfield.toLocaleLowerCase())
-        })
-        console.log('render');
-        return (
-      <div className="tc">
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange= {this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
